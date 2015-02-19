1956097d2d3a3aa6e840067dbf4784cd7dbf7292
Christoph Jerolimov <jerolimov+git+2014@gmail.com>
2015-02-19 02:45:38 +0100

    Update website, create german version and add prizes.

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 16,
+        zoom: 14,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(50.941, 6.958), // Köln
